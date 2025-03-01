7edee3c14d371879e50d84208a10db0178dfa191
Alex Metelli <alex-metelli@gmx.com>
2025-03-01 09:11:01 +0800

    added deafault value for empty string

--- crates/eth-rlp-verify/src/eras/dencun.rs
+++ crates/eth-rlp-verify/src/eras/dencun.rs
@@ -96,18 +96,23 @@ impl BlockHeaderDencun {
             nonce,
             base_fee_per_gas: U256::from_str(&db_header.base_fee_per_gas.unwrap_or_default())
                 .unwrap(),
-            withdrawals_root: H256::from_str(&db_header.withdrawals_root.unwrap_or_default())
-                .unwrap(),
+            withdrawals_root: H256::from_str(&db_header.withdrawals_root.unwrap_or(
+                "0x0000000000000000000000000000000000000000000000000000000000000000".to_string(),
+            ))
+            .unwrap_or_default(),
             parent_beacon_block_root: H256::from_str(
-                &db_header
-                    .parent_beacon_block_root
-                    .unwrap_or("0x".to_string()),
+                &db_header.parent_beacon_block_root.unwrap_or(
+                    "0x0000000000000000000000000000000000000000000000000000000000000000"
+                        .to_string(),
+                ),
             )
             .unwrap_or_default(),
-            blob_gas_used: U256::from_str(&db_header.blob_gas_used.unwrap_or("0x".to_string()))
+            blob_gas_used: U256::from_str(&db_header.blob_gas_used.unwrap_or("0x0".to_string()))
                 .unwrap_or_default(),
-            excess_blob_gas: U256::from_str(&db_header.excess_blob_gas.unwrap_or("0x".to_string()))
-                .unwrap_or_default(),
+            excess_blob_gas: U256::from_str(
+                &db_header.excess_blob_gas.unwrap_or("0x0".to_string()),
+            )
+            .unwrap_or_default(),
         }
     }
 
